5cfcd07471288c0a1a68b495fe8cd695908b68f3
Bohdan <bogdanbalanuk88@gmail.com>
2023-04-03 20:57:30 +0300

    new file:   Objects/ball.png 	new file:   Objects/player.png 	new file:   "Objects/\320\237\320\276\320\273\320\265.png" 	modified:   README.md 	new file:   __pycache__/classes_and_objects.cpython-310.pyc 	new file:   __pycache__/classes_and_objects.cpython-311.pyc 	new file:   __pycache__/main.cpython-311.pyc 	new file:   __pycache__/menu.cpython-311.pyc 	new file:   classes_and_objects.py 	new file:   main.py 	new file:   menu.py
    	modified:   README.md
    	modified:   __pycache__/classes_and_objects.cpython-311.pyc
    	modified:   __pycache__/menu.cpython-311.pyc
    	modified:   classes_and_objects.py
    	modified:   main.py
    	modified:   menu.py

---
+++ main.py
@@ -0,0 +1,82 @@
+from pygame import*
+from time import time as time1
+from random import randint 
+from classes_and_objects import* 
+import sys
+
+init()
+clock = time.Clock()
+
+window = display.set_mode((1050, 750))
+display.set_caption('Dino-Pong')
+background = transform.scale(image.load("Objects/Поле.png"), (1050, 750))
+window.blit(background, (0,0))
+
+gameover = False
+
+while not gameover:
+    for ev in event.get():
+        if ev.type == QUIT:
+            gameover = True
+        
+        if ev.type == KEYDOWN:
+            if ev.key == K_w:
+                pl1 = 1
+            if ev.key == K_s:
+                pl1 = -1
+
+            if ev.key == K_UP:
+                pl2 = 1
+            if ev.key == K_DOWN:
+                pl2 = -1
+        
+        if ev.type == KEYUP:
+            if ev.key == K_w:
+                pl1 = 0
+            if ev.key == K_s:
+                pl1 = 0
+            
+            if ev.key == K_UP:
+                pl2 = 0
+            if ev.key == K_DOWN:
+                pl2 = 0
+
+    if pl1 == 1 and player1.rect.y >= 0:
+        player1.move_Up() 
+    if pl1 == -1 and player1.rect.y <= 600:
+        player1.move_Down() 
+
+    if pl2 == 1 and player2.rect.y >= 0:
+        player2.move_Up() 
+    if pl2 == -1 and player2.rect.y <= 600:
+        player2.move_Down() 
+
+    if ball_direction_x == 1:
+        ball.rect.x -= ball.speed
+    if ball_direction_x == 2:
+        ball.rect.x += ball.speed
+
+    if ball_direction_y == 1:
+        ball.rect.y += ball.speed
+    if ball_direction_y == 2:
+        ball.rect.y -= ball.speed
+
+    if ball.rect.y >= 500:
+        ball_direction_y == 2
+    if ball.rect.y <= 0:
+        ball_direction_y == 1
+
+    if ball.colliderect(player1):
+        ball_direction_x = 2
+    if ball.colliderect(player2):
+        ball_direction_x = 1
+
+    
+    
+    window.blit(background, (0,0))
+    player1.draw_player(window)
+    player2.draw_player(window)   
+    ball.draw_ball(window)    
+
+    display.update()
+    clock.tick(120)
---
+++ menu.py
@@ -0,0 +1,44 @@
+from PyQt5.QtCore import*
+from PyQt5.QtWidgets import*
+
+app = QApplication([])
+main_window = QWidget()
+main_window.setWindowTitle("Меню")
+main_window.resize(600, 200)
+
+main_layout = QVBoxLayout()
+layout1 = QVBoxLayout()
+welcom_label = QLabel("Dino-Pong")
+choose_label = QLabel("Виберіть рівень складності:")
+
+button_easy = QPushButton("Легкий")
+button_middle = QPushButton("Середный")
+button_hard = QPushButton("Складний")
+
+layout1.addWidget(welcom_label, alignment= Qt.AlignCenter)
+layout1.addWidget(choose_label, alignment= Qt.AlignCenter)
+layout1.addWidget(button_easy)
+layout1.addWidget(button_middle)
+layout1.addWidget(button_hard)
+
+main_layout.addLayout(layout1)
+
+def choose_button_easy():
+    ball_speed = 1
+
+
+def choose_button_middle():
+    ball_speed = 2
+
+
+def choose_button_hard():
+    ball_speed = 3
+
+
+button_easy.clicked.connect(choose_button_easy)
+button_middle.clicked.connect(choose_button_middle)
+button_hard.clicked.connect(choose_button_hard)
+
+main_window.setLayout(main_layout)
+main_window.show()
+app.exec_()
